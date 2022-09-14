7528202bf04d8ba663d843fcdd9ab4b8eccf6fed
jonpfluger <jonpfluger718@gmail.com>
2022-09-13 21:20:52 -0500

    defined variables for characters

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-// Assignment Code
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+function generatePassword() {
+  lengthquestion = prompt("How many characters do you want your password to be? (Between 8 and 128 digits)")
+
+  lower = confirm("Do you want your password to include lowercase characters?");
+  
+  upper = confirm("Do you want your password to include uppercase characters?");
+  
+  numbers = confirm("Do you want your password to include numbers?");
+  
+  special = confirm("Do you want your password to include special characters?");
+}
+
+// var result = "";
+
+// if else
+// return result;
+
+
+
+
+
+
+
+
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input

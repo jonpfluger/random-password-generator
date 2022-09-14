3e00a13967423c9b61abbd50e26b401f7b588a23
jonpfluger <jonpfluger718@gmail.com>
2022-09-14 18:31:35 -0500

    fixed html on click that was causing it to run twice

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Assignment Code
-var lowerChars = ["abcdefghijklmnopqrstuvwxyz"];
-var upperChars = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numberChars = ["0123456789"];
-var specialChars = ["!#$%&()*+,-./:;<=>?@[]^_`{|}~"];
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 // prompts the user to choose a password length
 function generatePassword() {
@@ -29,49 +32,38 @@ function generatePassword() {
 
     if (upper === true && numbers === true && special === true) {
       var all = lowerChars.concat(upperChars, numberChars, specialChars);
-      console.log(all);
       var randomAll = all[Math.floor(Math.random() * all.length)];
-      console.log(randomAll);
       combined += randomAll;
-      console.log(combined);
 
     } else if (upper === true && numbers === true && special === false) {
       var lowerUpperNumber = lowerChars.concat(upperChars, numberChars);
-      console.log(lowerUpperNumber);
       var randomLowerUpperNumber = lowerUpperNumber[Math.floor(Math.random() * lowerUpperNumber.length)];
-      console.log(randomLowerUpperNumber);
       combined += randomLowerUpperNumber;
-      console.log(combined);
 
     } else if (upper === true && numbers === false && special === true) {
       var lowerUpperSpecial = lowerChars.concat(upperChars, specialChars);
-      console.log(lowerUpperSpecial);
-    //  var randomLowerUpperSpecial = lowerUpperSpecial[Math.floor(Math.random() * lowerUpperSpecial.length)];
-    //  combined += randomLowerUpperSpecial;
+      var randomLowerUpperSpecial = lowerUpperSpecial[Math.floor(Math.random() * lowerUpperSpecial.length)];
+      combined += randomLowerUpperSpecial;
 
     } else if (upper === false && numbers === true && special === true) {
       var lowerNumberSpecial = lowerChars.concat(numberChars, specialChars);
-      console.log(lowerNumberSpecial);
-    //  var randomLowerNumberSpecial = lowerNumberSpecial[Math.floor(Math.random() * lowerNumberSpecial.length)];
-    //  combined += randomLowerNumberSpecial;
+      var randomLowerNumberSpecial = lowerNumberSpecial[Math.floor(Math.random() * lowerNumberSpecial.length)];
+      combined += randomLowerNumberSpecial;
 
     } else if (upper === false && numbers === false && special === true) {
       var lowerSpecial = lowerChars.concat(specialChars);
-      console.log(lowerSpecial);
-    //  var randomLowerSpecial = lowerSpecial[Math.floor(Math.random() * lowerSpecial.length)];
-    //  combined += randomLowerSpecial;
+      var randomLowerSpecial = lowerSpecial[Math.floor(Math.random() * lowerSpecial.length)];
+      combined += randomLowerSpecial;
 
     } else if (upper === false && numbers === true && special === false) {
       var lowerNumber = lowerChars.concat(numberChars);
-      console.log(lowerNumber);
-    //  var randomLowerNumber = lowerNumber[Math.floor(Math.random() * lowerNumber.length)];
-    //  combined += randomLowerNumber;
+      var randomLowerNumber = lowerNumber[Math.floor(Math.random() * lowerNumber.length)];
+      combined += randomLowerNumber;
 
     } else if (upper === true && numbers === false && special === false) {
       var lowerUpper = lowerChars.concat(upperChars);
-      console.log(lowerUpper);
-    //  var randomLowerUpper = lowerUpper[Math.floor(Math.random() * lowerUpper.length)];
-    //  combined += randomLowerUpper;
+      var randomLowerUpper = lowerUpper[Math.floor(Math.random() * lowerUpper.length)];
+      combined += randomLowerUpper;
 
     // } else if (upper === false && numbers === false && special === false) {
     //   var lower = lowerChars;
